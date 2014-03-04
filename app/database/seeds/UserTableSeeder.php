2c443fcbca5d29ea4ec173a6591e14b55dcb7a03
Rob van Bentem <robvanbentem@gmail.com>
2014-03-04 19:41:44 +0000

    changed seeds

--- app/database/seeds/UserTableSeeder.php
+++ app/database/seeds/UserTableSeeder.php
@@ -15,11 +15,6 @@ class UserTableSeeder extends Seeder
 
         User::create(array('name' => 'Rob', 'short_name' => 'RvB', 'enabled' => 1));
         User::create(array('name' => 'Rurik', 'short_name' => 'RW', 'enabled' => 1));
-        User::create(array('name' => 'Arjan', 'short_name' => 'AS', 'enabled' => 1));
-        User::create(array('name' => 'Joshua', 'short_name' => 'JvD', 'enabled' => 1));
-        User::create(array('name' => 'Maarten', 'short_name' => 'MT', 'enabled' => 1));
-        User::create(array('name' => 'Ernstjan', 'short_name' => 'EJ', 'enabled' => 1));
-        User::create(array('name' => 'Henri', 'short_name' => 'HvL', 'enabled' => 1));
-        User::create(array('name' => 'Dienet', 'short_name' => 'DE', 'enabled' => 1));
+        User::create(array('name' => 'Luuk', 'short_name' => 'LH', 'enabled' => 1));
     }
 }
